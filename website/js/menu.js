ebc2c5ebf4c7701c0fbb4e56e3b26e8f6547b28d
Max Prudhomme <account@maxprudhomme.com>
2022-04-16 18:49:09 +0200

    Navbar Improvement
    Navbar overall improvement and effect adding.

--- website/js/menu.js
+++ website/js/menu.js
@@ -5,14 +5,14 @@ function scrollFunction()
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) 
     { 
         document.getElementById("projectLogo").style.width = "200px";
-        document.getElementById("projectLogo").style.height = "75px";
-        document.getElementById("mainMenu").style.marginTop = "10px";
+        document.getElementById("projectLogo").style.height = "80px";
+        document.getElementById("mainMenu").style.marginTop = "20px";
     } 
     else 
     { 
         document.getElementById("projectLogo").style.width = "400px";
-        document.getElementById("projectLogo").style.height = "150px";
-        document.getElementById("mainMenu").style.marginTop = "80px";
+        document.getElementById("projectLogo").style.height = "160px";
+        document.getElementById("mainMenu").style.marginTop = "60px";
     }
 }
 
