69020b1ba5000b36f565710cc042b408dc6fa002
Max Prudhomme <account@maxprudhomme.com>
2022-04-19 19:22:45 +0200

    Newsletter front / back Implementation
    Newsletter frontend and backend implementation.

---
+++ website/js/index.js
@@ -0,0 +1,33 @@
+function launcher()
+{
+    document.getElementById('submitArrow').style.marginLeft = '50px';
+    setTimeout(function(){
+        document.getElementById('submitArrow').style.display= "none"
+        document.getElementById('submitArrow').style.opacity= "0"
+        document.getElementById('checkmark').style.display= "block"
+        document.getElementById('submitArrow').style.marginLeft = '-50px';
+    },0500)
+    var result = { };
+    $.each($('#emailField').serializeArray(), function() {
+        result[this.name] = this.value;
+    });
+    $.ajax({
+        type: 'GET',
+        url: 'https://www.maxprudhomme.com/launchPlatform',
+        data: result,}).done(function(){
+                document.getElementById('checkmark').style.animation="fill .4s ease-in-out .4s forwards, scale .3s ease-in-out .9s both";
+                document.getElementById('checkmark__circle').style.animation="stroke 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards";
+                document.getElementById('checkmark__check').style.animation="stroke 0.3s cubic-bezier(0.65, 0, 0.45, 1) 0.8s forwards";
+            setTimeout(function(){
+                document.getElementById('checkmark').style.opacity= "0"
+            },2000)
+            setTimeout(function(){
+                document.getElementById('checkmark').style.display= "none"
+                document.getElementById('submitArrow').style.display= "block"
+                document.getElementById('submitArrow').style.opacity= "1"
+            },3000)
+            setTimeout(function(){
+                document.getElementById('submitArrow').style.marginLeft = '0px';
+            },3500)  
+        })
+}
