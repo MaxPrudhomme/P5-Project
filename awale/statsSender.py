3d797b376e1490a6848ce54335ce0120835d1513
Max Prudhomme <account@maxprudhomme.com>
2022-05-20 19:43:57 +0200

    Stats sender fixes
    Stats sender fixes.

--- awale/statsSender.py
+++ awale/statsSender.py
@@ -1,8 +1,8 @@
 from requests import ConnectionError, Timeout, TooManyRedirects, Session
 
 def statsSender(winner, looser, draw, duration):
-    url = "project.maxprudhomme.com/telemetryPlatform"
-    parameters = [winner, looser, draw, duration]
+    url = "https://project.maxprudhomme.com/statsPlatform"
+    parameters = {"winner": winner, "looser": looser, "draw": draw, "duration": duration}
     session = Session()
     try:
         response = session.get(url, params=parameters)
